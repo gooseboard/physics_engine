97001259680f16277ea0c4938297c2774a2c9826
adam <goose@Sufficients-MacBook-Pro.local>
2016-10-11 00:40:04 -0400

    fixed collision detection, still no momentum conservation

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class Actor:
-	def __init__(self,x_pos, y_pos, h, w,  xv, yv, color, x_prev, y_prev):
+	def __init__(self,x_pos, y_pos, h, w,  xv, yv, color):
 		self.x_pos = x_pos
 		self.y_pos = y_pos
 		self.h = h
@@ -7,11 +7,10 @@ class Actor:
 		self.xv = xv
 		self.yv = yv
 		self.color = color
-		self.y_prev = y_prev
-		self.x_prev = x_prev
+		
 
 
 class Rectangle(Actor):
-	def __init__(self, x_pos, y_pos, h, w,  xv, yv, color, x_prev, y_prev):
-		Actor.__init__(self, x_pos, y_pos, h, w,  xv, yv, color, x_prev, y_prev)
+	def __init__(self, x_pos, y_pos, h, w,  xv, yv, color):
+		Actor.__init__(self, x_pos, y_pos, h, w,  xv, yv, color)
 
